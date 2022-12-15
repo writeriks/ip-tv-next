8af0fd5d43bdc249684fce362a7a71b43846a902
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2022-12-15 18:10:39 +0100

    add login form events

--- components/login-modal/login-form.tsx
+++ components/login-modal/login-form.tsx
@@ -1,22 +1,35 @@
 import { Button } from '@chakra-ui/react'
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 
 import styles from '../../styles/LoginModal.module.scss'
 import LoginSection from './login-section'
 
+export interface LoginProps {
+  username: string
+  password: string
+  url: string
+}
+
 const LoginForm = () => {
+  const [formData, setFormData] = useState<LoginProps>({ username: '', password: '', url: '' })
+  console.log('🚀 ~ file: login-form.tsx:15 ~ LoginForm ~ formData', formData)
+
+  const onChange = useCallback(({ target }: React.ChangeEvent<HTMLInputElement>): void => {
+    const formValue = { [target.name]: target.value }
+
+    setFormData((prevFormData) => ({ ...prevFormData, ...formValue }))
+  }, [])
+
+  const onSubmitEvent = useCallback((event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+  }, [])
+
   return (
     <div className={styles.loginFormContainer}>
-      <form
-        onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
-          event.preventDefault()
-          console.log('asda')
-        }}
-        className={styles.loginForm}
-      >
-        <LoginSection label="username" />
-        <LoginSection label="password" />
-        <LoginSection label="url" />
+      <form onSubmit={onSubmitEvent} className={styles.loginForm} autoComplete="on">
+        <LoginSection label="username" value={formData.username} onChange={onChange} type="text" />
+        <LoginSection label="password" value={formData.password} onChange={onChange} type="password" />
+        <LoginSection label="url" value={formData.url} onChange={onChange} type="url" />
 
         <section>
           <div className={styles.submitButton}>
--- components/login-modal/login-section.tsx
+++ components/login-modal/login-section.tsx
@@ -5,14 +5,26 @@ import styles from '../../styles/LoginModal.module.scss'
 
 export interface LoginSectionProps {
   label: string
+  onChange: ({ target }: React.ChangeEvent<HTMLInputElement>) => void
+  value: string
+  type: React.HTMLInputTypeAttribute
 }
 
-const LoginSection: React.FC<LoginSectionProps> = ({ label }) => {
+const LoginSection: React.FC<LoginSectionProps> = ({ label, onChange, value, type }) => {
   return (
     <section>
       <label className={styles.inputLabel}>
         <a>{label}</a>
-        <Input focusBorderColor="#404186" className={styles.inputItem} placeholder={label} />
+        <Input
+          type={type}
+          onChange={onChange}
+          name={label}
+          value={value}
+          focusBorderColor="#404186"
+          className={styles.inputItem}
+          placeholder={label}
+          required
+        />
       </label>
     </section>
   )
