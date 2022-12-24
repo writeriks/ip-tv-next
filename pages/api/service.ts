7d370fd5bc8278ee58c5fb3760138d0d29dc9285
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2022-12-25 00:26:07 +0100

    test body parser

--- pages/api/service.ts
+++ pages/api/service.ts
@@ -17,13 +17,43 @@ const getChannelsFromApi = nc()
     if (response.status === 200) {
       const textResponse = await response.text()
       const channels = parser.parse(textResponse)
+
       res.status(200).json({ channels })
     }
   })
 
+// to check size of objects
+function roughSizeOfObject(object: any) {
+  const objectList = []
+  const stack = [object]
+  let bytes = 0
+
+  while (stack.length) {
+    const value = stack.pop()
+
+    if (typeof value === 'boolean') {
+      bytes += 4
+    } else if (typeof value === 'string') {
+      bytes += value.length * 2
+    } else if (typeof value === 'number') {
+      bytes += 8
+    } else if (typeof value === 'object' && objectList.indexOf(value) === -1) {
+      objectList.push(value)
+
+      for (const i in value) {
+        stack.push(value[i])
+      }
+    }
+  }
+  return bytes
+}
+
 export const config = {
   api: {
-    bodyParser: false, // Defaults to true. Setting this to false disables body parsing and allows you to consume the request body as stream or raw-body.
+    bodyParser: {
+      sizeLimit: '100mb',
+    },
+    /* bodyParser: false, */ // Defaults to true. Setting this to false disables body parsing and allows you to consume the request body as stream or raw-body.
     responseLimit: false, // Determines how much data should be sent from the response body. It is automatically enabled and defaults to 4mb.
     externalResolver: true, // Disables warnings for unresolved requests if the route is being handled by an external resolver like Express.js or Connect. Defaults to false.
   },
