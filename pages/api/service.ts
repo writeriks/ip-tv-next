13ebfae4748bd1edbd1722dfde2573c775c7d22d
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2022-12-24 23:30:29 +0100

    add limit change

--- pages/api/service.ts
+++ pages/api/service.ts
@@ -21,4 +21,12 @@ const getChannelsFromApi = nc()
     }
   })
 
+export const config = {
+  api: {
+    bodyParser: false, // Defaults to true. Setting this to false disables body parsing and allows you to consume the request body as stream or raw-body.
+    responseLimit: false, // Determines how much data should be sent from the response body. It is automatically enabled and defaults to 4mb.
+    externalResolver: true, // Disables warnings for unresolved requests if the route is being handled by an external resolver like Express.js or Connect. Defaults to false.
+  },
+}
+
 export default getChannelsFromApi
