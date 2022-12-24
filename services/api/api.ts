cea3263ad88542bd0819409850d6c94fd96b2bc1
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2022-12-24 23:50:56 +0100

    change api method

--- services/api/api.ts
+++ services/api/api.ts
@@ -1,6 +1,6 @@
 export const fetchChannels = async (username: string, password: string, url: string): Promise<Response> => {
   const response = await fetch(`/api/service?username=${username}&password=${password}&postUrl=${url}`, {
-    method: 'POST',
+    method: 'GET',
   })
   return response
 }
